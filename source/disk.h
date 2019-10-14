5c56205f96185d7b12d3e3e555ea2994c0bf0247
random532 <54436789+random532@users.noreply.github.com>
2019-10-14 20:01:01 +0200

    Add files via upload

---
+++ source/disk.h
@@ -0,0 +1,34 @@
+
+#include <unistd.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <math.h>
+#include <ctype.h>
+
+
+//#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <inttypes.h>
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+	#define D
+#else
+	#define D for(;0;)
+#endif
+
+//D prrintf(...);
+
+#define TT printf("test statement\n\n\n");
+
+//dbus-uuidgen --ensure
+
+
