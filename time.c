f5e736c418d636bad35183386081f2300b68b391
random532 <54436789+random532@users.noreply.github.com>
2020-10-31 21:53:50 +0100

    Add files via upload

--- time.c
+++ time.c
@@ -7,7 +7,7 @@ void timetab() {
 	timebox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 15);
 	gtk_container_add (GTK_CONTAINER (tab4), timebox);
 
-	gtk_container_add(GTK_CONTAINER (timebox), gtk_label_new("-------"));
+	gtk_container_add(GTK_CONTAINER (timebox), gtk_label_new("Not done yet."));
 
 	gtk_widget_show_all(timebox);
 }
