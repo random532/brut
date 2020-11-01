5ea384c6ac6506fb45625e7164e000b8ce5f2ff5
random532 <54436789+random532@users.noreply.github.com>
2020-11-01 21:46:16 +0100

    add comments.

--- disk.c
+++ disk.c
@@ -34,27 +34,34 @@ void scrolled() {
 
 void main_combo() {
 
+	/* 
+	 * A combo box with disk names.
+	 */
 	combo_toplevel = gtk_combo_box_text_new();
 
 	gtk_widget_set_focus_on_click(combo_toplevel, TRUE);
 	gtk_grid_attach(GTK_GRID (grid), GTK_WIDGET (combo_toplevel), 0, 0, 1, 1);
 	g_signal_connect (combo_toplevel, "changed", G_CALLBACK (on_toplevel_changed), NULL);
 
-	/* Add disk names to the box. */
+	/* Add disk names */
 	toplevel_entries();
 }
 
 void add_box() {
-	
+
+	/* 
+	 * In this box we pack a scrolled window (for treeviews), 
+	 * and the editor.
+	 */
 	box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
 	gtk_container_add(GTK_CONTAINER (diskbox), box);
 	
 	/* A window with scrollbars. */
 	scrolled();
 	
+	/* Separator */
 	GtkWidget *s1 = gtk_separator_new(GTK_ORIENTATION_VERTICAL);
-	gtk_container_add(GTK_CONTAINER (box), s1);
-	
+	gtk_container_add(GTK_CONTAINER (box), s1);	
 }
 
 void disk() {
@@ -64,8 +71,8 @@ void disk() {
 	gtk_container_add (GTK_CONTAINER (tab1), diskbox);
 
 	/* It has two boxes. */
-	add_grid();
-	add_box();
+	add_grid(); /* ... with a combo box and a refresh button. */
+	add_box();	/* ... with a treeview and the editor. */
 
 	/* Populate the scrolled window once. */
 	gtk_combo_box_set_active( GTK_COMBO_BOX (combo_toplevel), 0);
@@ -74,5 +81,4 @@ void disk() {
 
 	/* At last, add the editor. */
 	editor();
-
 }
