ddd52919a065c7a6683e4cc8d0ebcd368ee1b757
random532 <54436789+random532@users.noreply.github.com>
2021-07-17 13:05:36 +0200

    Add files via upload

---
+++ disks/disk.c
@@ -0,0 +1,101 @@
+/* Disk tab */
+#include "disk.h"
+
+/* Redraw (most of) the disk tab */
+void redraw_cb (GtkMenuItem *item, gpointer user_data) {  
+	toplevel_entries();
+	gtk_combo_box_set_active( GTK_COMBO_BOX (combo_toplevel), 0);
+	gtk_widget_destroy(thegrid);
+	editor();
+	gtk_text_buffer_set_text(GTK_TEXT_BUFFER(gtk_text_view_get_buffer(GTK_TEXT_VIEW(logwindow))), "          ", 10);
+}
+
+void add_grid() {
+	
+	/* A grid */
+	grid = gtk_grid_new();
+//	gtk_grid_insert_column(GTK_GRID(grid), 3);
+	gtk_box_pack_start(GTK_BOX(diskbox), grid, FALSE, TRUE, 0);
+	gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
+	gtk_grid_set_column_spacing(GTK_GRID(grid), 7);
+	
+	/*
+	 * This grid has two entries.
+	 * 1. a combo box that lets users chose whether to display
+	 * an overview of all disks, or contents of a specific disk.
+	 */
+	main_combo();
+
+	/* 2. A refresh button. */
+	b = gtk_button_new_with_mnemonic(l.mrefresh);
+	gtk_grid_attach(GTK_GRID (grid), GTK_WIDGET (b), 1, 0, 1, 1);
+	g_signal_connect (b, "clicked", G_CALLBACK (redraw_cb), NULL);
+}
+
+void scrolled() {
+	
+	/* Scrolled window for the treeview */
+	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
+                                    GTK_POLICY_ALWAYS, GTK_POLICY_AUTOMATIC); 
+	gtk_box_pack_start(GTK_BOX(box), scrolled_window, FALSE, TRUE, 0);
+	gtk_scrolled_window_set_propagate_natural_height (GTK_SCROLLED_WINDOW(scrolled_window), TRUE);
+	gtk_scrolled_window_set_propagate_natural_width (GTK_SCROLLED_WINDOW(scrolled_window), TRUE);
+}
+
+void main_combo() {
+
+	/* 
+	 * A combo box with disk names.
+	 */
+	combo_toplevel = gtk_combo_box_text_new();
+
+	gtk_widget_set_focus_on_click(combo_toplevel, TRUE);
+	gtk_grid_attach(GTK_GRID (grid), GTK_WIDGET (combo_toplevel), 0, 0, 1, 1);
+	g_signal_connect (combo_toplevel, "changed", G_CALLBACK (on_toplevel_changed), NULL);
+
+	/* Add disk names */
+	toplevel_entries();
+}
+
+void add_box() {
+
+	/* 
+	 * In this box we pack a scrolled window (for treeviews), 
+	 * and the editor.
+	 */
+	box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+	gtk_container_add(GTK_CONTAINER (diskbox), box);
+	
+	/* A window with scrollbars. */
+	scrolled();
+	
+	/* Separator */
+	GtkWidget *s1 = gtk_separator_new(GTK_ORIENTATION_VERTICAL);
+	gtk_container_add(GTK_CONTAINER (box), s1);	
+}
+
+void disk() {
+	
+	if(GTK_IS_WIDGET(diskbox)) {
+		gtk_widget_destroy(diskbox);
+		tree = NULL;
+		tree1 = NULL;
+	}
+	
+	/* A top level container. */
+	diskbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 15);
+	gtk_container_add (GTK_CONTAINER (tab1), diskbox);
+
+	/* It has two boxes. */
+	add_grid(); /* ... with a combo box and a refresh button. */
+	add_box();	/* ... for the treeview and the editor. */
+
+	/* Populate the scrolled window once. */
+	gtk_combo_box_set_active( GTK_COMBO_BOX (combo_toplevel), 0);
+
+	gtk_widget_show_all(diskbox);
+
+	/* At last, add the editor. */
+	editor();
+}
---
+++ disks/on_combos_changed.c
@@ -0,0 +1,252 @@
+#include "disk.h"
+
+void on_toplevel_changed() {
+
+	/* update a disk view */
+	
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_toplevel));
+	if(string == NULL) {
+		/* gtk_combo_box_remove_all() creates a signal for each removed entry */
+		/* this function is called multiple times */
+		//msg("Error! gtk_combo_box_text_get_active_text is empty!");
+		return;
+	}
+
+	/* we don't need these any more */
+	if(tree != NULL) {
+		gtk_widget_destroy(tree);
+		tree = NULL;
+	}
+	if(tree1 != NULL) {
+		gtk_widget_destroy(tree1);
+		tree1 = NULL;
+	}
+
+	/* what did user select? */
+	if(strncmp(string, l.overview, 25) == 0) {
+		/* display an overview of all disks */
+		free(string);
+		tree = create_treeview();
+		populate_treeview();
+		gtk_widget_show(tree);
+	}
+	else {
+		/* display a particular disk */
+		tree1 = create_treeview1();
+		populate_treeview1(string); /* do we need to check return value? */
+		gtk_widget_show(tree1);
+		free(string);
+	}
+}
+
+void toplevel_entries() {
+	
+	/* cleanup */
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(combo_toplevel));
+	gtk_combo_box_text_append( GTK_COMBO_BOX_TEXT (combo_toplevel), 0, l.overview);
+	
+	/* refresh the list of disks */
+	if(all_disks != NULL)
+		free(all_disks);
+	all_disks = get_disks();
+	if(all_disks == NULL) {
+		msg("no disks found!");		
+		return;
+	}
+
+	/* add each disk to the combo box */
+	char *brk;
+	char *one_disk = strtok_r( all_disks, " ", &brk);
+
+	while( one_disk != NULL ) {
+		gtk_combo_box_text_append( GTK_COMBO_BOX_TEXT (combo_toplevel), NULL, one_disk);
+		one_disk = strtok_r( NULL, " ", &brk);
+	}
+}
+
+void on_geom_changed() {
+
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_geom));
+	if(string == NULL)
+		return;
+	
+	/* update disk information */
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(combo_disks));
+	add_geoms();
+	add_slices();
+	add_partitions();	
+		
+	/* show only relevant widgets */
+	hide_widgets();
+	
+	if(strcmp(string, "create") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_disks));
+		gtk_widget_show(GTK_WIDGET (combo_schemes));
+	}
+
+	else if(strcmp(string, "destroy") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_disks));
+	}
+	else if(strcmp(string, "add") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_disks));
+		gtk_widget_show(GTK_WIDGET (text_size));
+		gtk_widget_show(GTK_WIDGET (text_alignment));
+	}
+	else if(strcmp(string, "delete") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+	}
+	else if(strcmp(string, "modify") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+	}
+	else if(strcmp(string, "set") == 0) { 
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+		gtk_widget_show(GTK_WIDGET (combo_bootoptions));
+	}
+	else if(strcmp(string, "unset") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+		gtk_widget_show(GTK_WIDGET (combo_bootoptions));
+	}
+	else if(strcmp(string, "resize") == 0) { 
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+		gtk_widget_show(GTK_WIDGET (text_size));
+		gtk_widget_show(GTK_WIDGET (text_alignment));
+	}
+	else if(strcmp(string, "bootcode") == 0) { 
+		gtk_widget_show(GTK_WIDGET (combo_bootcode));
+		gtk_widget_show(GTK_WIDGET (gfile));
+	}
+	else if(strcmp(string, "file system") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+		gtk_widget_show(GTK_WIDGET (combo_filesystems));
+		gtk_widget_show(GTK_WIDGET (text_label));
+	}
+}
+
+void on_fs_changed() {
+
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_filesystems));
+	if(string == NULL)
+		return;
+		
+	/* hide all options */
+	gtk_widget_hide(GTK_WIDGET (toggle_soft));
+	gtk_widget_hide(GTK_WIDGET (toggle_journal));
+	gtk_widget_hide(GTK_WIDGET (toggle_fast));
+	gtk_widget_hide(GTK_WIDGET (toggle_comp));
+
+	/* ... but show relevant ones */
+	if(strcmp(string, "ufs2") == 0) {
+		gtk_widget_show(GTK_WIDGET (toggle_soft));
+		gtk_widget_show(GTK_WIDGET (toggle_journal));
+	}
+
+	else if(strcmp(string, "ntfs") == 0) {
+		gtk_widget_show(GTK_WIDGET (toggle_fast));
+		gtk_widget_show(GTK_WIDGET (toggle_comp));
+	}
+}
+
+void on_bootcode_changed() {
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_bootcode));
+	if(string == NULL)
+		return;
+		/* hide all widgets */
+	gtk_widget_hide(GTK_WIDGET (combo_disks));
+	gtk_widget_hide(GTK_WIDGET (combo_partitions));
+	
+	if(strcmp(string, "Partition") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_partitions));
+	}
+	else if(strcmp(string, "Disk") == 0) {
+		gtk_widget_show(GTK_WIDGET (combo_disks));
+	}
+}
+
+void on_disks_changed() {
+	
+	/* 
+	 * If "gpart add" then show only partition types that fit 
+	 * onto the scheme.
+	 */
+	 
+	 	const gchar *gdisk = get_combo_box_disk(0);
+	if( gdisk == NULL)
+		return;
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_geom));
+	if(string == NULL)
+		return;
+	if( strncmp(string, "add", 3) != 0 )
+		return;	
+
+	char *scheme = get_scheme(gdisk);
+	if(scheme == NULL) {
+		msg("Error fetching partitioning scheme. Weird.");
+		gtk_widget_destroy(thegrid);
+		editor();
+		return;
+	}
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(combo_types) );
+	add_types(scheme);
+	if(( strncmp(scheme, "GPT", 3) == 0 ) || ( strncmp(scheme, "BSD", 3) == 0 ) )
+		gtk_widget_show(GTK_WIDGET (text_label));
+	else
+		gtk_widget_hide(GTK_WIDGET (text_label) );
+	free(scheme);
+	gtk_widget_show(GTK_WIDGET (combo_types));
+}
+
+
+void on_partitions_changed() {
+	
+	/* 
+	 * If "gpart modify" then show only partition types that fit 
+	 * onto the scheme.
+	 */
+	  
+	char *string;
+	string = gtk_combo_box_text_get_active_text( GTK_COMBO_BOX_TEXT (combo_geom));
+	if(string == NULL)
+		return;
+	if( strncmp(string, "modify", 6) != 0 )
+		return;
+		
+	char *gpartition = get_combo_box_partition(0);
+	if(gpartition == NULL)
+		return;
+	find_p(gpartition);
+	char *scheme = get_scheme(gpartition);
+	if(scheme == NULL) {
+		msg("error fetching partitioning scheme.Weird.");
+		gtk_widget_destroy(thegrid);
+		editor();
+		return;
+	}
+	free(gpartition);
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(combo_types) );
+	add_types(scheme);
+	if(( strncmp(scheme, "GPT", 3) == 0 ) || ( strncmp(scheme, "BSD", 3) == 0 ) )
+		gtk_widget_show(GTK_WIDGET (text_label));
+	else
+		gtk_widget_hide(GTK_WIDGET (text_label) );
+	free(scheme);
+	gtk_widget_show(GTK_WIDGET (combo_types));
+	
+}
+
+void on_scheme_changed() {
+	
+	/**/
+	const gchar *gscheme = get_combo_box_scheme();
+	if (gscheme == NULL)
+		return;
+
+	if(strncmp(gscheme, "GPT", 3) == 0)
+		gtk_widget_show(text_entries);
+	else
+		gtk_widget_hide(text_entries);
+}
